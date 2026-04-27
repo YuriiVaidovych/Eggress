bf61ae9b190c7eb8b9720e57e688aade1da4c870
YuriiVaidovych <v.yura.ua@gmail.com>
2026-04-27 16:48:20 +0300

    feat: setup clean architecture layers and Hilt DI

---
+++ Eggress/app/src/main/java/com/yuriivaidovych/eggressapp/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.yuriivaidovych.eggressapp.di
+
+import android.content.Context
+import com.yuriivaidovych.eggressapp.data.AppDatabase
+import com.yuriivaidovych.eggressapp.data.AppDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        // Створюємо окремий Scope для бази даних, щоб він не залежав від екранів
+        val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+        return AppDatabase.getDatabase(context, applicationScope)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDao(database: AppDatabase): AppDao {
+        return database.appDao()
+    }
+}
---
+++ Eggress/app/src/main/java/com/yuriivaidovych/eggressapp/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.yuriivaidovych.eggressapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yuriivaidovych.eggressapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
